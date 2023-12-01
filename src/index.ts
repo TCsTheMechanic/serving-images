f6185a3885a5c555faee67db9c9972f2589d1d63
Eduardo Cerutti <ceruttioliveira@hotmail.com>
2023-12-01 17:15:29 -0300

    Create new index

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
-import express, { Application } from "express"
-import { join } from "path"
 import dotenv from "dotenv"
+import app from "./app"
 
 dotenv.config()
 
 const port = process.env.API_PORT
-const app: Application = express()
-
-app.use(express.static(join("./images")))
 
 app.listen(port, () => {
   console.log("Image server is Up!!")
